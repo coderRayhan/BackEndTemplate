41181dbb93ef6649c5f9937bbfcf3f8f805c66ca
rayhanju007 <coder.rayhan07@gmail.com>
2023-10-22 00:38:25 +0600

    user dto and account controller added

---
+++ src/Web/Controllers/Identity/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Identity.Account.Queries;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers.Identity;
+public class AccountController : BaseApiController
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    public AccountController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Register(UserDto user)
+    {
+
+        return null;
+    }
+}
